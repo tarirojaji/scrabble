a0154e6cb14c5fab1c152f94ee7928b6892a0818
jaji <57304962+tarirojaji@users.noreply.github.com>
2021-01-31 06:10:03 +0000

    tw, tl squares added

--- test/index.js
+++ test/index.js
@@ -3,6 +3,8 @@ const cellWrap = document.querySelector('.cell-wrap')
 
 
 
+
+
 function createBoard(size) {
     
     for (let i = 0; i<size*size; i++) {
@@ -14,9 +16,11 @@ function createBoard(size) {
         cellWrap.style.width = `${39*size}px`
         cellWrap.style.height = `${39*size}px`
         }
+
 }
 
 createBoard(15);
+
 
 
 // function clickA() {
@@ -79,29 +83,66 @@ clickAttacher('.cell', 'active')
 //   }
 // }
 
-let tW3 = {
+let TW = {
   name: 'triple',
   cords: [0, 7, 14, 105, 119, 210, 217, 224,]
+  // 0, 7, 105 mirror these indexes
 }
 
-console.log(tW3.cords)
 
-// function bl() {
-//   if (7 in tW3.cords) {
-//   console.log(tW3)
+// let cellIdx = {
+//   'ct': [112,],
+//   'tw': [0, 7, 105],
+//   'tl': [20, 76, 80],
+//   'dw': [16, 32, 48, 64],
+//   'dl': [3, 36, 45, 52, 92, 96, 108]
+// };
+
+let cellIdx = {
+  'ct': [112,],
+  'tw': [0, 7, 14, 105, 119, 210, 217, 224,],
+  'tl': [20, 24, 76, 80, 84, 88,136, 140, 144, 148, 215, 219],
+  // 'dw': [16, 32, 48, 64],
+  // 'dl': [3, 36, 45, 52, 92, 96, 108]
+};
+
+for (const e of cellIdx.ct) {
+  if (112 === e) {
+    console.log('yes')
+  }
+}
+// for (let i = 0; i <= 14; i++) {
+//   for (let j = 0; j <= 14; j++) {
+//     let ci = [i,j]
+//     // console.log(box);
 //   }
 // }
 
-// bl();
 
-function test(selector, cn, text) {
+function test(selector, cn, text, sp, style) {
   const elem = document.querySelectorAll(selector);
-  for (const c of tW3.cords) {
+  for (const c of sp) {
       // const elem = document.querySelectorAll(selector);
       elem[c].classList.add(cn)
+      elem[c].style.backgroundColor = style
       elem[c].textContent = text.toUpperCase();
       // i+=9-3
       }
 }
 
-test('.cell', 'active', 'triple word score')
+test('.cell', 'active', '⭑', cellIdx.ct, 'salmon')
+test('.cell', 'active', 'TW', cellIdx.tw, '#a9687d')
+test('.cell', 'active', 'TL', cellIdx.tl, '#6881a9')
+
+
+// let prCells = [(r='',"T2d3T3d2T.D3t3t3D3D3d.d3D2d2D3d3D2d4D5D5t3t3t3t3d3d.d3d2T2d3".replace(/./g,c=>(c=+c?'.'.repeat(c):c,r=c+r,c))+'X'+r).match(/.{15}/g).join`
+// `]
+// // console.log(f)
+
+// for (let i = 0;i<2;i++) {
+//   for (const pc of prCells) {
+//       if (pc.indexOf(pc[i]) in cellIdx)
+//       console.log(ff[i])
+//   }
+// }
+
