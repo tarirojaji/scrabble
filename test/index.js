ec4490e7469213983db409d97b792cad7ecab750
jaji <57304962+tarirojaji@users.noreply.github.com>
2021-01-31 18:40:09 +0000

    all premium cells

--- test/index.js
+++ test/index.js
@@ -3,13 +3,11 @@ const cellWrap = document.querySelector('.cell-wrap')
 
 
 
-
-
 function createBoard(size) {
     
     for (let i = 0; i<size*size; i++) {
         const test = document.createElement('div')
-        test.className = 'cell'
+        test.classList.add('cell')
         cellWrap.appendChild(test)
         // board.style.gridTemplateColumns = `repeat(${size}, auto)`
         cellWrap.style.gridTemplateColumns = `repeat(${size}, auto)`
@@ -17,37 +15,30 @@ function createBoard(size) {
         cellWrap.style.height = `${39*size}px`
         }
 
+
+
 }
 
 createBoard(15);
 
 
 
-// function clickA() {
-//     const cell = document.querySelectorAll('.cell')
-//     for (const c of cell) {
-//         c.addEventListner('click', () => {
-//         c.classList.toggle('on')
-//     });
-//     }
-// }
 
-// clickA();
-
-
-function clickAttacher(selector, cn) {
-    const elem = document.querySelectorAll(selector);
+// function clickAttacher(selector, cn) {
+//     const elem = document.querySelectorAll(selector);
   
-    for (const e of elem) {
-      e.addEventListener('click', () => {
-        e.classList.toggle(cn)
-        // e.textContent = '*'
-        console.log(elem[1])
-      });
-    }
-  }
+//     for (const e of elem) {
+//       e.addEventListener('click', () => {
+//         e.classList.toggle(cn)
+//         // e.textContent = '*'
+//         console.log(elem[1])
+//       });
+//     }
+//   }
 
-clickAttacher('.cell', 'active')
+// clickAttacher('.cell', 'active')
+
+
 
 
 // function test(selector, cn, text) {
@@ -67,29 +58,6 @@ clickAttacher('.cell', 'active')
 // test('.cell', 'active', 'triple word score')
 
 
-// name every single square
-// assign classes to matching square
-
-// const pos = {
-//   tW3: {
-//     A1: 0,
-//     H1: 7,
-//     O1: 14,
-//     A8: 98,
-//     O8: 112,
-//     A15: 214,
-//     H15: 234,
-//     O15: 249,
-//   }
-// }
-
-let TW = {
-  name: 'triple',
-  cords: [0, 7, 14, 105, 119, 210, 217, 224,]
-  // 0, 7, 105 mirror these indexes
-}
-
-
 // let cellIdx = {
 //   'ct': [112,],
 //   'tw': [0, 7, 105],
@@ -100,26 +68,14 @@ let TW = {
 
 let cellIdx = {
   'ct': [112,],
-  'tw': [0, 7, 14, 105, 119, 210, 217, 224,],
+  'tw': [0, 7, 14, 105, 119, 210, 217, 224],
   'tl': [20, 24, 76, 80, 84, 88,136, 140, 144, 148, 215, 219],
-  // 'dw': [16, 32, 48, 64],
-  // 'dl': [3, 36, 45, 52, 92, 96, 108]
+  'dw': [16, 28, 32, 42, 48, 56, 64, 70, 154, 160, 168, 176, 182, 192, 196, 208],
+  'dl': [3, 11, 36, 38, 45, 52, 59, 92, 96, 98, 102, 108, 116,122, 126, 128,132, 165, 172, 179, 186, 188, 213, 221],
 };
 
-for (const e of cellIdx.ct) {
-  if (112 === e) {
-    console.log('yes')
-  }
-}
-// for (let i = 0; i <= 14; i++) {
-//   for (let j = 0; j <= 14; j++) {
-//     let ci = [i,j]
-//     // console.log(box);
-//   }
-// }
 
-
-function test(selector, cn, text, sp, style) {
+function premiumCells(selector, cn, text, sp, style) {
   const elem = document.querySelectorAll(selector);
   for (const c of sp) {
       // const elem = document.querySelectorAll(selector);
@@ -130,9 +86,11 @@ function test(selector, cn, text, sp, style) {
       }
 }
 
-test('.cell', 'active', '⭑', cellIdx.ct, 'salmon')
-test('.cell', 'active', 'TW', cellIdx.tw, '#a9687d')
-test('.cell', 'active', 'TL', cellIdx.tl, '#6881a9')
+premiumCells('.cell', 'premium', '⭑', cellIdx.ct, '#a9687d')
+premiumCells('.cell', 'premium', 'TW', cellIdx.tw, '#a9687d')
+premiumCells('.cell', 'premium', 'TL', cellIdx.tl, '#023785')
+premiumCells('.cell', 'premium', 'DL', cellIdx.dl, '#5c77a1')
+premiumCells('.cell', 'premium', 'DW', cellIdx.dw, 'salmon')
 
 
 // let prCells = [(r='',"T2d3T3d2T.D3t3t3D3D3d.d3D2d2D3d3D2d4D5D5t3t3t3t3d3d.d3d2T2d3".replace(/./g,c=>(c=+c?'.'.repeat(c):c,r=c+r,c))+'X'+r).match(/.{15}/g).join`
