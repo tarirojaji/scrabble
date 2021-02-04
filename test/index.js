519d2c6ebfae210a67c0b63947829f11b5652731
jaji <57304962+tarirojaji@users.noreply.github.com>
2021-02-04 22:09:08 +0000

    layout update & modules added

--- test/index.js
+++ test/index.js
@@ -1,85 +1,39 @@
-const board = document.querySelector('#board');
+import { cellPos, premiumObj } from './modules/boardPos.js';
+
+
+const board = document.querySelector('#board'); //currently not in use
 const cellWrap = document.querySelector('.cell-wrap');
+const slotWrap = document.querySelector('.slot-wrap');
 const boardSize = 15;
 
 
 
 // 
 
-const boardCoords = [
-    4,0,0,1,0,0,0,4,0,0,0,1,0,0,4,
-    0,3,0,0,0,2,0,0,0,2,0,0,0,3,0,
-    0,0,3,0,0,0,1,0,1,0,0,0,3,0,0,
-    1,0,0,3,0,0,0,1,0,0,0,3,0,0,1,
-    0,0,0,0,3,0,0,0,0,0,3,0,0,0,0,
-    0,2,0,0,0,2,0,0,0,2,0,0,0,2,0,
-    0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,
-    4,0,0,1,0,0,0,5,0,0,0,1,0,0,4,
-    0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,
-    0,2,0,0,0,2,0,0,0,2,0,0,0,2,0,
-    0,0,0,0,3,0,0,0,0,0,3,0,0,0,0,
-    1,0,0,3,0,0,0,1,0,0,0,3,0,0,1,
-    0,0,3,0,0,0,1,0,1,0,0,0,3,0,0,
-    0,3,0,0,0,2,0,0,0,2,0,0,0,3,0,
-    4,0,0,1,0,0,0,4,0,0,0,1,0,0,4,
-];
-
-
-
-const premiumObj = {
-  1: {
-    name: 'DL',
-    color: '#5c77a1',
-    index: [
-      3, 11, 36, 38, 45, 52, 59, 92, 96, 98, 102,108, 116, 
-      122, 126, 128,132, 165, 172, 179, 186, 188, 213, 221
-    ],
-  },
-  2: {
-    name: 'TL',
-    color: '#023785',
-    index: [20, 24, 76, 80, 84, 88,136, 140, 144, 148, 215, 219],
-  },
-  3: {
-    name: 'DW',
-    color: '#e49288',
-    index: [16, 28, 32, 42, 48, 56, 64, 70, 154, 160, 168, 176, 182, 192, 196, 208],
-  },
-  4: {
-    name: 'TW',
-    color: '#a9687d',
-    index: [0, 7, 14, 105, 119, 210, 217, 224],
-  },
-  5: {
-    name: '⭑',
-    color: '#a9687d',
-    index: [112],
-  },
-};
-
-function create(size) {
+function createBoard(size) {
   for (let i = 0; i<size*size; i++) {
   const div = document.createElement('div')
     div.classList.add('cell')
     cellWrap.appendChild(div)
     cellWrap.style.gridTemplateColumns = `repeat(${size}, auto)`
-    cellWrap.style.width = `${39*size}px`
-    cellWrap.style.height = `${39*size}px`
+    cellWrap.style.width = `${29*size}px`
+    cellWrap.style.height = `${29*size}px`
   };
+
 
   function premiumStyles() {
     const cell = document.querySelectorAll('.cell');
 
       for (let i=0; i<size*size; i++) {
         for (const key in premiumObj) {
-          if (boardCoords[i] !== 0) {
+          if (cellPos[i] !== 0) {
             cell[i].classList.add('premium')
-          } if (boardCoords[i] == key) {
+          } if (cellPos[i] == key) {
             cell[i].style.backgroundColor = premiumObj[key].color
             cell[i].textContent = premiumObj[key].name
 
             // console.log(`i: ${i}, key: ${key}, 
-            // board name: ${boardCoords[i]}, ${premiumObj[key].name}`);
+            // board name: ${cellPos[i]}, ${premiumObj[key].name}`);
           }
         }
       };
@@ -87,10 +41,55 @@ function create(size) {
 
   premiumStyles();
 
+
+
+  function slots() {
+    for (let i = 0; i<7; i++) {
+      const slot = document.createElement('div')
+        slot.classList.add('slot')
+        slotWrap.appendChild(slot)
+        // slotWrap.style.gridTemplateColumns = `repeat(${size}, auto)`
+        // slotWrap.style.width = `${29*size}px`
+        // slotWrap.style.height = `${29*size}px`
+      };
+    
+  }
+  
+  slots();
+
 };
 
-create(boardSize);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+createBoard(boardSize);
 
 
 
@@ -100,11 +99,12 @@ function clickAttacher(selector, cn) {
     for (const e of elem) {
       e.addEventListener('click', () => {
         e.classList.toggle(cn)
+        console.log('clicked')
       });
     }
   };
 
 clickAttacher('.cell', 'active');
-
+clickAttacher('.slot', 'active');
 
 
