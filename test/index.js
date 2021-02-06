43bd7d7cb4c7a19cbb22bcfea30fd0f51015b318
jaji <57304962+tarirojaji@users.noreply.github.com>
2021-02-06 14:41:36 +0000

    changes

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
 import { cellPos, premiumObj } from './modules/boardPos.js';
+// import { dragStartHandler } from './modules/drag.js';
+
 
 
 const board = document.querySelector('#board'); //currently not in use
@@ -7,17 +9,22 @@ const slotWrap = document.querySelector('.slot-wrap');
 const boardSize = 15;
 
 
+// array of letters for testing
+let arr = ['D','I','O','J','O','J','O']
+
 
 // 
 
 function createBoard(size) {
   for (let i = 0; i<size*size; i++) {
   const div = document.createElement('div')
-    div.classList.add('cell')
+    div.classList.add('cell', 'dropzone')
     cellWrap.appendChild(div)
     cellWrap.style.gridTemplateColumns = `repeat(${size}, auto)`
-    cellWrap.style.width = `${29*size}px`
-    cellWrap.style.height = `${29*size}px`
+    // NOTE: Loop & rename ID to eg. 00,01,02,etc or a1,a2...d5,d6,etc.
+    div.id = `cl${i}`
+    // cellWrap.style.width = `${29*size}px`
+    // cellWrap.style.height = `${29*size}px`
   };
 
 
@@ -42,12 +49,14 @@ function createBoard(size) {
   premiumStyles();
 
 
-
   function slots() {
     for (let i = 0; i<7; i++) {
       const slot = document.createElement('div')
         slot.classList.add('slot')
+        slot.id = `cl${i+size*size}`
         slotWrap.appendChild(slot)
+        slot.setAttribute('draggable', true)
+        slot.textContent = arr[i]
         // slotWrap.style.gridTemplateColumns = `repeat(${size}, auto)`
         // slotWrap.style.width = `${29*size}px`
         // slotWrap.style.height = `${29*size}px`
@@ -58,35 +67,6 @@ function createBoard(size) {
   slots();
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 createBoard(boardSize);
@@ -108,3 +88,104 @@ clickAttacher('.cell', 'active');
 clickAttacher('.slot', 'active');
 
 
+
+// improve this function??? improve all the funcs lololol
+function startClick() {
+  const btnStart = document.querySelector('.btnStart');
+  const slot = document.querySelectorAll('.slot');
+
+  for (const s of slot) {
+    btnStart.addEventListener('click', () => {
+    btnStart.textContent = `SET`;
+    s.style.visibility = 'visible'
+    console.log('clicked')
+    });
+  }
+
+}
+
+startClick();
+
+
+
+
+// function shuffleClick() {
+//   const btnShuffle = document.querySelector('.btnShuffle');
+//   const slot = document.querySelectorAll('.slot');
+
+//   for (const s of slot) {
+//     btnShuffle.addEventListener('click', () => {
+//       for (let i = array.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//         s.textContent = arr[i]
+//     }
+
+//     });
+//   }
+
+// }
+
+// shuffleClick();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function dragStartHandler(e) {
+  const data = e.target.id;
+  e.dataTransfer.setData('text/plain', data);
+
+}
+
+function dragOverHandler(e) {
+  e.preventDefault();
+}
+
+function dropHandler(e) {
+  const data = e.dataTransfer.getData('text/plain');
+  const dragged = document.getElementById(data);
+  e.currentTarget.append(dragged);
+}
+
+const dropzones = document.querySelectorAll('.dropzone');
+for (const dropzone of dropzones) {
+  dropzone.addEventListener('dragover', dragOverHandler);
+  dropzone.addEventListener('drop', dropHandler);
+}
+
+const divs = document.querySelectorAll('div');
+for (const div of divs) {
+  div.addEventListener('dragstart', dragStartHandler);
+}
+
+
+
+
+
+
+// NOTES: export/import drag function/s 
+//        stop special cells text being pushed out after drap/drop
+//        fix cell-wrapper from vanishing after drag and drop
+//        edit cell ids / adjust dropzone to cell-wrapper only..if possible
+//        have a look at z-index for positioning
---
+++ test/modules/drag.js
@@ -0,0 +1,32 @@
+function dragStartHandler(e) {
+  const data = e.target.id;
+  e.dataTransfer.setData('text/plain', data);
+
+}
+
+function dragOverHandler(e) {
+  e.preventDefault();
+}
+
+function dropHandler(e) {
+  const data = e.dataTransfer.getData('text/plain');
+  const dragged = document.getElementById(data);
+  e.currentTarget.append(dragged);
+}
+
+const dropzones = document.querySelectorAll('.dropzone');
+for (const dropzone of dropzones) {
+  dropzone.addEventListener('dragover', dragOverHandler);
+  dropzone.addEventListener('drop', dropHandler);
+}
+
+const divs = document.querySelectorAll('div');
+for (const div of divs) {
+  div.addEventListener('dragstart', dragStartHandler);
+}
+
+
+export { dragStartHandler, dragOverHandler, dropHandler, };
+
+
+// figure out how to import this files!
