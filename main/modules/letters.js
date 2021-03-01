3d46a5a113f2da64ed98b64ab322670f5ddd63d3
jaji <57304962+tarirojaji@users.noreply.github.com>
2021-03-01 12:17:11 +0000

    letters

---
+++ main/modules/letters.js
@@ -0,0 +1,129 @@
+
+export let letters = {
+    a: {
+      tiles: 10,
+      value: 1
+    },
+    b: {
+      tiles: 2,
+      value: 4
+    },
+    c: {
+      tiles: 2,
+      value: 4
+    }, 
+    d: {
+      tiles: 5,
+      value: 2
+    }, 
+    e: {
+      tiles: 12,
+      value: 1
+    }, 
+    f: {
+      tiles: 2,
+      value: 4
+    },  
+    g: {
+      tiles: 3,
+      value: 3
+    }, 
+    h: {
+      tiles: 4,
+      value: 3
+    },
+    i: {
+      tiles: 9,
+      value: 1
+    },
+    j: {
+      tiles: 1,
+      value: 10
+    },
+    k: {
+      tiles: 1,
+      value: 5
+    },
+    l: {
+      tiles: 4,
+      value: 1
+    },
+    l: {
+      tiles: 4,
+      value: 1
+    },  
+    m: {
+      tiles: 2,
+      value: 3
+    },  
+    n: {
+      tiles: 5,
+      value: 2
+    },
+    n: {
+      tiles: 5,
+      value: 2
+    },
+    o: {
+      tiles: 6,
+      value: 1
+    },
+    
+    p: {
+      tiles: 2,
+      value: 4
+    },
+    
+    q: {
+      tiles: 1,
+      value: 10
+    },
+    r: {
+      tiles: 6,
+      value: 1
+    },
+  
+    s: {
+      tiles: 5,
+      value: 1
+    },
+  
+    t: {
+      tiles: 7,
+      value: 1
+    },
+    
+    u: {
+      tiles: 4,
+      value: 2
+    },
+    
+    v: {
+      tiles: 2,
+      value: 4
+    },
+    
+    w: {
+      tiles: 2,
+      value: 4
+    },
+    
+    x: {
+      tiles: 1,
+      value: 4
+    },
+    
+    y: {
+      tiles: 2,
+      value: 4
+    },
+    z: {
+      tiles: 1,
+      value: 10
+    },
+    '_': {
+      tiles: 2,
+      value: 0
+    }
+  };
+  
